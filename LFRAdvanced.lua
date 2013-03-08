fcdcab5262eeb5cd9fdcd1acc32df9e3aa438921
TOM_RUS <tom_rus@github.com>
2013-03-09 02:19:56 +0400

    Real 5.2 fix Added player spec in tooltip

--- LFRAdvanced.lua
+++ LFRAdvanced.lua
@@ -1,7 +1,7 @@
 NAME_ILVL_TEMPLATE = "|c%s%s %s (%.02f)|r";
 
 function MyFunction(self, ...)
-    local name, level, areaName, className, comment, partyMembers, status, class, encountersTotal, encountersComplete, isIneligible, isLeader, isTank, isHealer, isDamage, talentPoints, spec1, spec2, spec3, isLFM, Armor, SpellDamage, SpellHeal, CritMelee, CritRanged, CritSpell, MP5, MP5Combat, AttackPower, Agility, Health, Mana, Unk1, avgILVL, Unk2, Dodge, Block, Parry, Haste, Expertise = SearchLFGGetResults(self.index);
+    local name, level, areaName, className, comment, partyMembers, status, class, encountersTotal, encountersComplete, isIneligible, isLeader, isTank, isHealer, isDamage, talentPoints, spec, isLFM, Armor, SpellDamage, SpellHeal, CritMelee, CritRanged, CritSpell, MP5, MP5Combat, AttackPower, Agility, Health, Mana, Unk1, avgILVL, Unk2, Dodge, Block, Parry, Haste, Expertise = SearchLFGGetResults(self.index);
     GameTooltip:SetOwner(self, "ANCHOR_RIGHT", 27, -37);
 
     if ( partyMembers > 0 ) then
@@ -18,15 +18,15 @@ function MyFunction(self, ...)
         GameTooltip:AddLine("\n"..IMPORTANT_PEOPLE_IN_GROUP);
         for i=1, partyMembers do
             -- SearchLFGGetPartyResults also returns "isLeader ... Expertise" fields as SearchLFGGetResults does
-            local name, level, relationship, className, areaName, comment, isLeader, isTank, isHealer, isDamage, talentPoints, spec1, spec2, spec3, isLFM, Armor, SpellDamage, SpellHeal, CritMelee, CritRanged, CritSpell, MP5, MP5Combat, AttackPower, Agility, Health, Mana, Unk1, avgILVL, Unk2, Dodge, Block, Parry, Haste, Expertise = SearchLFGGetPartyResults(self.index, i);
+            local name, level, relationship, className, areaName, comment, isLeader, isTank, isHealer, isDamage, talentPoints, spec, isLFM, Armor, SpellDamage, SpellHeal, CritMelee, CritRanged, CritSpell, MP5, MP5Combat, AttackPower, Agility, Health, Mana, Unk1, avgILVL, Unk2, Dodge, Block, Parry, Haste, Expertise = SearchLFGGetPartyResults(self.index, i);
             if ( relationship ) then
                 if ( relationship == "ignored" ) then
-                    GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, spec1, spec2, spec3, className, avgILVL, RED_FONT_COLOR), IGNORED, RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b, RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b);
+                    GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, spec, className, avgILVL, RED_FONT_COLOR), IGNORED, RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b, RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b);
                 elseif ( relationship == "friend" ) then
-                    GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, spec1, spec2, spec3, className, avgILVL, GREEN_FONT_COLOR), FRIEND, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b);
+                    GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, spec, className, avgILVL, GREEN_FONT_COLOR), FRIEND, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b);
                 end
             else
-                GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, spec1, spec2, spec3, className, avgILVL, NORMAL_FONT_COLOR), PLAYER, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b);
+                GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, spec, className, avgILVL, NORMAL_FONT_COLOR), PLAYER, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b);
             end
         end
     else
@@ -68,7 +68,7 @@ function MyFunction(self, ...)
         GameTooltip:AddLine("\n"..ALL_BOSSES_ALIVE);
     end
 
-    GameTooltip:AddLine(GetPlayerInfoString(level, class, spec1, spec2, spec3, className));
+    GameTooltip:AddLine(GetPlayerInfoString(level, class, spec, className));
 
     GameTooltip:AddLine("Extra info:");
 
@@ -151,8 +151,9 @@ function MyFunction(self, ...)
     GameTooltip:Show();
 end
 
-function GetSpecString(spec1, spec2, spec3)
-    return format("%u/%u/%u", spec1, spec2, spec3);
+function GetSpecString(spec)
+    local _, spec = GetSpecializationInfoByID(spec);
+    return spec;
 end
 
 function GetClassColorString(class)
@@ -170,13 +171,13 @@ function ColorToString(color)
     return format("ff%.2x%.2x%.2x", color.r * 255, color.g * 255, color.b * 255);
 end
 
-function GetPlayerInfoString(level, class, spec1, spec2, spec3, className)
-    return format(PLAYER_LEVEL, level, GetClassColorString(class), GetSpecString(spec1, spec2, spec3), className)
+function GetPlayerInfoString(level, class, spec, className)
+    return format(PLAYER_LEVEL, level, GetClassColorString(class), GetSpecString(spec), className)
 end
 
-function GetPlayerInfoStringWithIlvl(name, level, spec1, spec2, spec3, className, ilvl, color)
+function GetPlayerInfoStringWithIlvl(name, level, spec, className, ilvl, color)
     local colorStr = ColorToString(color);
-    local str = format(PLAYER_LEVEL, level, colorStr, GetSpecString(spec1, spec2, spec3), className);
+    local str = format(PLAYER_LEVEL, level, colorStr, GetSpecString(spec), className);
     return format(NAME_ILVL_TEMPLATE, colorStr, name, str, ilvl);
 end
 
