883faf56e0f902626405c6b006284f9d63df60b4
TOM_RUS <tom_rus@github.com>
2013-12-10 22:41:24 +0400

    Role icons

--- LFRFrameHacks.lua
+++ LFRFrameHacks.lua
@@ -37,40 +37,55 @@ function MyLFRBrowseButton_OnEnter(self)
 		GameTooltip:AddLine(GetPlayerInfoString(level, specID, className));
 		GameTooltip:AddTexture("Interface\\LFGFrame\\LFGRole", 0, 0.25, 0, 1);
 
-		GameTooltip:AddLine(format(LFM_NUM_RAID_MEMBER_TEMPLATE, partyMembers));
+		GameTooltip:AddLine(format(LFM_NUM_RAID_MEMBER_TEMPLATE, partyMembers + 1));
 		-- Bogus texture to fix spacing
 		GameTooltip:AddTexture("");
 
 		--Display party members.
 		local displayedMembersLabel = false;
-		local groupILevel = avgILevel;
+		local groupILevel = 0;
 		local groupMembers = 0;
-		for i=1, partyMembers do
+		for i=0, partyMembers do
 			local name, level, relationship, className, areaName, comment, isLeader, isTank, isHealer, isDamage, bossKills, specID, isGroupLeader, armor, spellDamage, plusHealing, CritMelee, CritRanged, critSpell, mp5, mp5Combat, attackPower, agility, maxHealth, maxMana, gearRating, avgILevel, defenseRating, dodgeRating, BlockRating, ParryRating, HasteRating, expertise = SearchLFGGetPartyResults(realIndex or self.index, i);
 			if name then
 				groupILevel = groupILevel + avgILevel;
 				groupMembers = groupMembers + 1
-				if ( relationship ) then
-					if ( not displayedMembersLabel ) then
-						displayedMembersLabel = true;
-						GameTooltip:AddLine("\n"..IMPORTANT_PEOPLE_IN_GROUP);
-					end
-					if ( relationship == "ignored" ) then
-						GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, specID, className, avgILevel, RED_FONT_COLOR_CODE), IGNORED, RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b, RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b);
-					elseif ( relationship == "friend" ) then
-						GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, specID, className, avgILevel, GREEN_FONT_COLOR_CODE), FRIEND, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b);
-					end
-				else
-					if IsGuildie(name) then
-						GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, specID, className, avgILevel, GREEN_FONT_COLOR_CODE), GUILD, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b);
+				if groupMembers < 20 then--limit to 20 members in tooltip
+					if ( relationship ) then
+						if ( not displayedMembersLabel ) then
+							displayedMembersLabel = true;
+							GameTooltip:AddLine("\n"..IMPORTANT_PEOPLE_IN_GROUP);
+							GameTooltip:AddTexture("");
+						end
+						if ( relationship == "ignored" ) then
+							GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, specID, className, avgILevel, RED_FONT_COLOR_CODE), IGNORED, RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b, RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b);
+						elseif ( relationship == "friend" ) then
+							GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, specID, className, avgILevel, GREEN_FONT_COLOR_CODE), FRIEND, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b);
+						end
 					else
-						GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, specID, className, avgILevel, NORMAL_FONT_COLOR_CODE), PLAYER, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b);
+						if IsGuildie(name) then
+							GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, specID, className, avgILevel, GREEN_FONT_COLOR_CODE), GUILD, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b);
+						else
+							GameTooltip:AddDoubleLine(GetPlayerInfoStringWithIlvl(name, level, specID, className, avgILevel, NORMAL_FONT_COLOR_CODE), PLAYER, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b);
+						end
+					end
+					--if ( isLeader ) then
+					--	GameTooltip:AddTexture("Interface\\LFGFrame\\LFGRole", 0, 0.25, 0, 1);
+					--end
+					if ( isTank ) then
+						GameTooltip:AddTexture("Interface\\LFGFrame\\LFGRole", 0.5, 0.75, 0, 1);
+					end
+					if ( isHealer ) then
+						GameTooltip:AddTexture("Interface\\LFGFrame\\LFGRole", 0.75, 1, 0, 1);
+					end
+					if ( isDamage ) then
+						GameTooltip:AddTexture("Interface\\LFGFrame\\LFGRole", 0.25, 0.5, 0, 1);
 					end
 				end
 			end
 		end
 		if groupILevel > 0 then
-			GameTooltip:AddLine("Group avg ilvl: "..groupILevel/(groupMembers+1));
+			GameTooltip:AddLine("Group avg ilvl: "..groupILevel/groupMembers);
 		end
 	else
 		GameTooltip:AddLine(name);
