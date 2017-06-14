571fc18a600e4177be92b63c84ac1507431cd02c
TOM_RUS <tom_rus@github.com>
2017-06-14 17:02:34 +0300

    Fix

--- LFGListHacks.lua
+++ LFGListHacks.lua
@@ -10,9 +10,9 @@ LFGListFrame.SearchPanel.ResultsInset:SetPoint("TOPLEFT", -1, -102);
 
 LFGListFrame.SearchPanel.SearchBox:HookScript("OnTextChanged", function(self, userInput)
 	local text = self:GetText();
-	if (text and text ~= "") or userInput then
-		LFRAdvancedOptions.LastSearchText = text;
-	end
+	--if (text and text ~= "") or userInput then
+	LFRAdvancedOptions.LastSearchText = text or "";
+	--end
 end)
 
 LFGListFrame.CategorySelection.FindGroupButton:SetScript("OnClick", function(self)
